8cc925ffd8c6e93f4be54c33596a985b73632a00
Asutosh Rath <asutosh.rath@eatechpvtltd.com>
2026-02-11 12:50:32 +0530

    Update llms.py change llm

--- app/services/llms.py
+++ app/services/llms.py
@@ -6,8 +6,8 @@ from app.env_settings import env
 class LLMService:
     def __init__(self):
         # Centralized Model Setting
-        # self.model_name = "llama-3.3-70b-versatile" 
-        self.model_name = "meta-llama/llama-4-maverick-17b-128e-instruct"
+        self.model_name = "llama-3.3-70b-versatile" 
+        # self.model_name = "meta-llama/llama-4-maverick-17b-128e-instruct"
         
         self.groq_client = ChatGroq(api_key=env.GROQ_API_KEY, model=self.model_name)
         # self.mistral_client = ChatMistralAI(api_key=env.MISTRAL_API_KEY, model="mistral-large-latest")
